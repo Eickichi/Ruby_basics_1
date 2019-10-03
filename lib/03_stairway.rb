3dc13dfe644e750deca7af9386ac7ad57621b59a
Eickichi <Davidye75019@gmail.com>
2019-10-04 00:38:13 +0200

    Finish last_exo

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,27 +1,40 @@
-#LANCE LE DÉ
-stair = 0
-while stair != 10
+def launch_dice
   dice = rand(1..6)
-  puts dice
-  if dice == 2 || dice == 3 || dice == 4
-    puts "Tu n'avance pas"
-    puts "Tu est a l'étage #{stair}"
-  elsif dice == 5 || dice == 6
-    puts "Tu avance"
-    stair = stair + 1
-    puts "Tu est a l'étage #{stair}"
-  elsif dice == 1
-    puts "Tu descend"
-    stair = stair - 1
-    puts "Tu est a l'étage #{stair}"
+  return dice
+end
+
+def game_logic
+stair = 0
+dice = 0
+  while stair != 10
+      dice = launch_dice
+      puts dice
+    if dice == 2 || dice == 3 || dice == 4
+      puts "Tu n'avance pas"
+      puts "Tu est a l'étage #{stair}"
+    elsif dice == 5 || dice == 6
+      puts "Tu avance"
+      stair = stair + 1
+      puts "Tu est a l'étage #{stair}"
+    elsif dice == 1
+      puts "Tu descend"
+      stair = stair - 1 if stair > 0
+      puts "Tu est a l'étage #{stair}"
+    end
   end
-  if stair == -1
-    stair == 0
+  return stair
+end
+
+def game_win(stair)
+  if stair == 10
+    puts "Bravo"
+  else
+    "T'es nul"
   end
 end
 
-if stair == 10
-  puts "Bravo"
-else
-  "T'es nul"
+def perform
+  game_win(game_logic)
 end
+
+perform
